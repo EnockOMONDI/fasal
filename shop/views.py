317e9b307b9970fd88db79bf842a7fdc5cef9853
EnockOMONDI <enockomondike@gmail.com>
2019-06-11 05:06:36 +0300

    student view

--- shop/views.py
+++ shop/views.py
@@ -6,15 +6,15 @@ from .models import Category, Student
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Student.objects.filter(available=True)
+    students = Student.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Student.objects.filter(category=category)
+        students = Student.objects.filter(category=category)
 
     context = {
         'category': category,
         'categories': categories,
-        'products': products
+        'students': students
     }
     return render(request, 'shop/product/list.html', context)
 
