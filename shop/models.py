df65821cf4fa78aeec05046c47c32a0baa3daf25
EnockOMONDI <enockomondike@gmail.com>
2019-11-27 01:53:46 +0300

    BUG [altered db]

--- shop/models.py
+++ shop/models.py
@@ -6,8 +6,8 @@ from imagekit.processors import ResizeToFill
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True ,db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True ,db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -25,8 +25,8 @@ class Category(models.Model):
 
 class Student(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True)
     brief_story = models.TextField(blank=True)
     perfomance_card = models.FileField(upload_to='documents/student_perfomance_cards')
     document = models.FileField(upload_to='documents/fee_statements')
@@ -50,8 +50,8 @@ class Student(models.Model):
         return reverse('shop:product_detail', args=[self.id, self.slug])
     
 class Media(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True)
     image = models.ImageField(blank=True, null=True, upload_to='media/')
     image_thumbnail = ImageSpecField(source='image',
                                  processors=[ResizeToFill(350, 350)],
@@ -69,10 +69,10 @@ class Media(models.Model):
         return reverse('shop:media_detail', args=[self.id, self.slug])
 
 class Team(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    position = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
+    position = models.CharField(max_length=255, db_index=True)
     brief_description = models.TextField(blank=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True)
     image = models.ImageField(blank=True, null=True, upload_to='media/')
     image_thumbnail = ImageSpecField(source='image',
                                  processors=[ResizeToFill(350, 350)],
